5ec6687a978d416f35c1c6102ec678b6242429cc
Himalaya <yugmani@yahoo.com>
2021-03-21 13:29:52 -0700

    Comments:ADD

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function removeTransition(e) {
+  //skip it if it's not a transform
   if (e.propertyName !== "transform") return;
+  //If it is a transform, then do it...
   e.target.classList.remove("playing");
 }
 
@@ -7,10 +9,10 @@ function playMusic(e) {
   const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
 
-  if (!audio) return;
+  if (!audio) return; //if not audio, stop the function from running
 
   key.classList.add("playing");
-  audio.currentTime = 0;
+  audio.currentTime = 0; //Rewind to the start;
   audio.play();
 }
 
